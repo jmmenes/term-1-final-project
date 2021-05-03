58dc7b6e0c5d90c1620f18947d18719a49fe1862
John Meneses <jmmenes90@gmail.com>
2021-05-03 10:25:17 -0400

    Added a Yoga tutorial section. Made more tweaks to HTML & CSS. Updated instructions & README file.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# term-1-final-project
-Meditation &amp; relaxation web app
+# Woo Sah, Mobile App
+
+&nbsp;
+
+## Motivation
+
+I believe mental health is just as important as physical health. You need both of those.
+
+&nbsp;
+
+## Why
+
+Purpose of app: To help people meditate, do yoga (stretch), or relax with a selection of curated music & sounds. We live in a noisy modern world full of distractions, stress, & constant bad news. This can hopefully be a tool to help people cope with their days if necessary.
+
+&nbsp;
+
+## Usage
+
+The user can read an instructions pop up by clicking on a button on the mobile home page.
+The use can also just click on an activity button of their choice & just hit play on the music. The music will keep playing until the tracks are finished or stop button is pressed.
+
+&nbsp;
+
+### Yoga Tutorial
+
+The user can read a quick explanation. The user will then choose a yoga pose from the dropdown menu and once a selection has been made and "enter" is clicked, the user will see the name of the yoga pose in its original Sanskrit language at the top of the page, followed by a simple illustration of the pose and a how-to video for that pose.
+
+&nbsp;
+
+## How It Was Built
+
+HTML
+CSS
+JavaScript
+jQuery Mobile
+jQuery
+YouTube for the music & API
+API: https://github.com/rebeccaestes/yoga_api
+VS Code
---
+++ api.js
@@ -0,0 +1,92 @@
+//
+var youtube_Key = "AIzaSyBcY7xBC8qSNeUQv39-csOZ6sWqitZ1BJw";
+
+// Access Yoga JSON API
+function listOfAllYogaPoses() {
+  fetch("https://raw.githubusercontent.com/rebeccaestes/yoga_api/master/yoga_api.json")
+    .then((result) => result.json())
+    .then((newResult) => {
+      getVariableforAllPoses(newResult);
+      var newResult = newResult;
+    })
+    .catch((error) => console.log(error));
+}
+
+// Access YouTube API
+function callYoutubeAPI(valueSelected) {
+  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=how+to+do+${valueSelected}+yoga+pose&maxResults=1&&safeSearch=moderate&key=${youtube_Key}`)
+    .then((youtubeResult) => youtubeResult.json())
+    .then((youtubeResult) => {
+      displayOtherResults(youtubeResult.items[0]);
+    })
+    .catch((error) => console.log(error));
+}
+
+// Create Dropdown Menu
+function getVariableforAllPoses(newResult) {
+  for (let i = 0; i < newResult.length; i++) {
+    let allPoses = newResult[i].english_name;
+    $(".select-dropdown").append(
+      `
+      <option class="dropdown-style" id="move-dropdown" value="${allPoses}">
+        ${allPoses}
+      </option>
+      `
+    );
+    if (allPoses === newResult[i].english_name) {
+      let sanskritName = newResult[i].sanskrit_name;
+      let poseImage = newResult[i].img_url;
+    }
+  }
+  submitButton(newResult);
+}
+
+// Enter/Submit Button
+function submitButton(newResult) {
+  $(".dropdown-form").submit((event) => {
+    event.preventDefault();
+    let valueSelected = $(".dropdown-style:selected").val();
+    callYoutubeAPI(valueSelected);
+    for (let i = 0; i < newResult.length; i++) {
+      if (valueSelected === newResult[i].english_name) {
+        let objectSelected = newResult[i];
+        cssChanges();
+        displayResults(objectSelected);
+      }
+    }
+  });
+}
+
+// jQuery changes to CSS
+function cssChanges() {
+  $("#yoga-explanation").hide();
+  $("#intro-background").hide();
+}
+
+// Results Displayed - Yoga
+function displayResults(objectSelected) {
+  $(".results").empty();
+  $(".results").append(`
+      <div class='position-rel'>
+      <div id="sanskrit-div"><p>${objectSelected.sanskrit_name}</p></div>
+      </div>
+      <div class='results-inline'>
+      <img id="pose-img" src="${objectSelected.img_url}"></img>
+      </div>
+
+    `);
+}
+
+// Results Displayed - YouTube
+function displayOtherResults(video) {
+  $(".results-inline").append(`
+      <iframe width="500" height="315" src="https://www.youtube.com/embed/${video.id.videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    `);
+}
+
+// START HERE
+function init() {
+  listOfAllYogaPoses();
+}
+
+$(init); //document on ready
